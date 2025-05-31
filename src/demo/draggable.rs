8abddc0dae4a6bc845c433c65c31c425cc0369af
Colin <zephyr.raine@gmail.com>
2025-06-01 01:45:10 +0200

    drag

--- src/demo/draggable.rs
+++ src/demo/draggable.rs
@@ -2,19 +2,17 @@ use bevy::prelude::*;
 #[derive(Component)]
 pub struct Draggable;
 
-
-
 pub(super) fn plugin(app: &mut App) {
     app.add_observer(on_drag);
 }
 
-
 fn on_drag(
     drag: Trigger<Pointer<Drag>>,
     mut transforms: Query<(&mut Transform, &Draggable)>,
+    time: Res<Time>,
 ) {
     if let Ok((mut transform, _draggable)) = transforms.get_mut(drag.target) {
-        transform.translation += Vec3::new(drag.delta.x, 0.0, drag.delta.y);
+        let sign = drag.delta.x.signum() * drag.delta.length() * 0.5;
+        transform.translation += Vec3::new(sign * time.delta_secs(), 0.0, 0.0);
     }
 }
-
