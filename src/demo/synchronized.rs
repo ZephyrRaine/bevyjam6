25f1e26a4395081be69a98e7aa4e56e1022678d9
Colin <zephyr.raine@gmail.com>
2025-06-01 01:09:07 +0200

    clean shit

--- src/demo/synchronized.rs
+++ src/demo/synchronized.rs
@@ -5,14 +5,12 @@ use crate::screens::Screen;
 #[derive(Resource, Reflect)]
 #[reflect(Resource)]
 pub struct SyncTracks {
-    timer_init: Timer,
     pub timer_tracks: [Timer; 3],
 }
 
 impl FromWorld for SyncTracks {
     fn from_world(_world: &mut World) -> Self {
         Self {
-            timer_init: Timer::from_seconds(0.0, TimerMode::Once),
             timer_tracks: [
                 Timer::from_seconds((60.0 / (90.0 / 8.0)) * 0.5, TimerMode::Repeating),
                 Timer::from_seconds((60.0 / (90.0 * 1.5)) * 0.5, TimerMode::Repeating),
@@ -40,10 +38,7 @@ pub(super) fn plugin(app: &mut App) {
 }
 
 pub fn update_timers(mut blink_tracks: ResMut<SyncTracks>, time: Res<Time>) {
-    blink_tracks.timer_init.tick(time.delta());
-    if blink_tracks.timer_init.finished() {
-        for timer in blink_tracks.timer_tracks.iter_mut() {
-            timer.tick(time.delta());
-        }
+    for timer in blink_tracks.timer_tracks.iter_mut() {
+        timer.tick(time.delta());
     }
 }
